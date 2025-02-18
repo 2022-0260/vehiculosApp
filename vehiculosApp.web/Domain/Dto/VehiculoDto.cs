dd4fa8e65999f9f17fc5eeac14ad39293578c09f
2022-0260 <2022-0260@uteco.edu.do>
2025-02-18 01:43:19 -0400

    Agregar archivos de proyecto.

---
+++ vehiculosApp.web/Domain/Dto/VehiculoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace vehiculosApp.web.Domain.Dto
+{
+    public class VehiculoDto
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+        public string Color { get; set; }
+
+    }
+}
